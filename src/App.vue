934fa76c6bc142294f85a9803d8e6a4657b7df60
Tony Kwong <tonykkn4321@gmail.com>
2025-07-16 10:48:51 +0800

    Completed htmlcssprimer

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4 class="bg-primary text-white text-center p-2">
+      Adam's To Do List
+    </h4>
+      <div class="container-fluid p-4">
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+        <div class="row" v-for="t in tasks" v-bind:key="t.action">
+          <div class="col">{{t.action}}</div>
+          <div class="col-2">{{t.done}}</div>
+        </div>
+      </div>
+  </div>
+  <div class="form-group m-2">
+    <label>New Item:</label>
+    <input v-model="newItemText" class="form-control" />
+  </div>
+    <div class="text-center">
+    <button class="btn btn-primary" v-on:click="addNewTodo">
+      Add
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: function () {
+    return {
+      tasks: [{ action: "Buy Flowers", done: false },
+              { action: "Get Shoes", done: false },
+              { action: "Collect Tickets", done: true },
+              { action: "Call Joe", done: false }],
+              newItemText: ""
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.tasks.push({
+        action: this.newItemText,
+        done: false
+      });
+        this.newItemText = "";
+      }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
